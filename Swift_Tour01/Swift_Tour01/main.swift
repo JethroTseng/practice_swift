179373d17a7161f40c88fd7a84cd06b0bca04de3
Jethro.Tseng <jethro.tseng@gmail.com>
2014-07-12 02:03:58 +0800

    Swift init

---
+++ Swift_Tour01/Swift_Tour01/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift_Tour01
+//
+//  Created by Ｍr. Charlie on 2014/7/12.
+//  Copyright (c) 2014年 JethroTseng. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
